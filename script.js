2db88635ebf067366ef58f16313ee58db98784d3
Pranit Dakhinkar <97027846+dakhinkar@users.noreply.github.com>
2023-01-16 10:10:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 function threeSum(arr, target) {
-  //your code here
-	arr = arr.sort((a,b)=> a-b);
-	console.log(arr);
+  arr = arr.sort((a,b) => a-b);
+//   console.log(arr);
+  let ans = Infinity;
+  
+  arr.forEach((a, i)=>{
+    let target2 = target-a;
+    
+    let end =arr.length-1;
+    let startIndex = i+1;
+    while(startIndex < end ){
+      let sum = arr[startIndex] + arr[end];
+      
+      if(Math.abs(target - sum+ a) <= Math.abs(target - ans)){
+        ans = sum+a;
+      }
+      if(sum > target2){
+        end--;
+      }else{
+        startIndex++;
+      }
+      
+    }
+    
+    
+  });
 
-	
+	console.log(ans);
 }
 
 module.exports = threeSum;
